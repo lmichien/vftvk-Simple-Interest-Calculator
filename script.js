e7b961e981c198558fa74b3742f0a83530e236e3
lmichienzi <lmichienzi@gmail.com>
2021-06-06 22:14:13 +0000

    Improved highlight

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("interest").innerText=interest ;
-    document.getElementById("result").innerHTML="If you deposit \<b\>"+ principal+"\</b\>, at an interest rate of \<b\>"+rate+"%\</b\>, You will receive an amount of \<b\>"+interest+"\</b\> in the year \<b\>"+year+"\<b\>" ;
+    document.getElementById("result").innerHTML="If you deposit <mark>"+ principal+"</mark>,<br/> at an interest rate of <mark>"+rate+"%</mark>,<br/> You will receive an amount of <mark>"+interest+"</mark><br/> in the year <mark>"+year+"</mark>" ;
     }
 };
 
