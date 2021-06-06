f2e369854c48d625a86a0a2189a7770b4fce3e24
lmichienzi <lmichienzi@gmail.com>
2021-06-06 22:02:15 +0000

    final assignament

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
-}
-        
+    var principal = document.getElementById("principal").value;
+    if (principal<=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else
+    {
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("interest").innerText=interest ;
+    document.getElementById("result").innerHTML="If you deposit \<b\>"+ principal+"\</b\>, at an interest rate of \<b\>"+rate+"%\</b\>, You will receive an amount of \<b\>"+interest+"\</b\> in the year \<b\>"+year+"\<b\>" ;
+    }
+};
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+};
