64e07630536c8d5d88cc3440c039562f52ead784
lmichienzi <lmichienzi@gmail.com>
2021-06-06 23:14:18 +0000

    Improved CSS

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("interest").innerText=interest ;
-    document.getElementById("result").innerHTML="If you deposit <mark>"+ principal+"</mark>,<br/> at an interest rate of <mark>"+rate+"%</mark>,<br/> You will receive an amount of <mark>"+interest+"</mark><br/> in the year <mark>"+year+"</mark>" ;
+    document.getElementById("result").innerHTML="If you deposit <mark>"+ principal+"</mark>,<br/> at an interest rate of <mark>"+rate+"%</mark>,<br/> You will receive an amount of <mark>"+interest+"</mark><br/> in the year <mark>"+year+"</mark><br/><br/>" ;
     }
 };
 
